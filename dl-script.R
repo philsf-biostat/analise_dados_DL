bc3a4802add9f72d4615790be17abfe2d42e7bfc
Felipe Figueiredo <philsf79@gmail.com>
2015-11-24 00:25:27 -0200

    * dl-script.R: Concentrando criação dos dataframes num único script.

--- dl-script.R
+++ dl-script.R
@@ -2,18 +2,24 @@ detach(margarida)
 margarida<-read.csv("BANCO PÓS QUALIFICAÇÃO 06_10_15.csv", header = T)
 
 # Guardando apenas as variáveis que serão utilizadas
-margarida<-margarida[c("Ordem","Sexo","Medicamento","VITAMINA.D.BASAL","VITAMINA.D.FINAL","ADIPONECTINA.BASAL","ADIPONECTINA.FINAL","CTX.BASAL","CTX.FINAL","FAO.BASAL","FAO.FINAL","LEPTINA.BASAL","LEPTINA.FINAL","OPG.BASAL","OPG.FINAL","TNF.ALFA.BASAL","TNF.ALFA.FINAL","CALCIO.BASAL","CALCIO.FINAL")]
+#margarida<-margarida[c("Ordem","Sexo","Medicamento","VITAMINAD.BASAL","VITAMINAD.FINAL","ADIPONECTINA.BASAL","ADIPONECTINA.FINAL","CTX.BASAL","CTX.FINAL","FAO.BASAL","FAO.FINAL","LEPTINA.BASAL","LEPTINA.FINAL","OPG.BASAL","OPG.FINAL","TNFALFA.BASAL","TNFALFA.FINAL","CALCIO.BASAL","CALCIO.FINAL")]
+margarida<-margarida[c("Ordem","Sexo","Medicamento","VITAMINAD.BASAL","VITAMINAD.FINAL","ADIPONECTINA.BASAL","ADIPONECTINA.FINAL","CTX.BASAL","CTX.FINAL","FAO.BASAL","FAO.FINAL","LEPTINA.BASAL","LEPTINA.FINAL","OPG.BASAL","OPG.FINAL","TNFALFA.BASAL","TNFALFA.FINAL")]
+
 # Retirando os pacientes do medicamento Protos
 margarida<-margarida[margarida$Medicamento!="Protos",]
 margarida<-droplevels(margarida)
-# # Corrigindo o paciente P24, outlier da adiponectina (0.7->0.17)
-# margarida[margarida$Ordem=="P24",]$ADIPONECTINA.BASAL<-0.17
+
 # Retirando o paciente P5, outlier do FAO basal
 margarida<-margarida[c(margarida$Ordem!="P5"),]
 
-str(margarida)
+#convertendo de wide para long
+margarida.wide <- margarida
+margarida.long <- reshape(margarida, varying=c("ADIPONECTINA.BASAL", "ADIPONECTINA.FINAL", "CTX.BASAL", "CTX.FINAL", "FAO.BASAL", "FAO.FINAL", "LEPTINA.BASAL","LEPTINA.FINAL","OPG.BASAL","OPG.FINAL","TNFALFA.BASAL","TNFALFA.FINAL", "VITAMINAD.BASAL", "VITAMINAD.FINAL"), idvar="Ordem", direction="long")
+# margarida<-margarida.long
+# rm(margarida)
 
-attach(margarida)
+attach(margarida.wide)
+attach(margarida.long)
 
 ## Gráficos descriticos
 # Boxplots
