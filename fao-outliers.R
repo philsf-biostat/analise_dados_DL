45db31b3cab7828df11244877e9e1887f1f4ece4
Felipe Figueiredo <philsf79@gmail.com>
2015-11-24 01:34:17 -0200

    * fao-outliers.R: limpeza

--- fao-outliers.R
+++ fao-outliers.R
@@ -1,18 +1,7 @@
-detach(margarida)
-margarida<-read.csv("BANCO PÓS QUALIFICAÇÃO 06_10_15.csv", header = T)
-
-# Guardando apenas as variáveis que serão utilizadas
-#margarida<-margarida[c("Ordem","Sexo","Medicamento","VITAMINA.D.BASAL","VITAMINA.D.FINAL","ADIPONECTINA.BASAL","ADIPONECTINA.FINAL","CTX.BASAL","CTX.FINAL","FAO.BASAL","FAO.FINAL","LEPTINA.BASAL","LEPTINA.FINAL","OPG.BASAL","OPG.FINAL","TNF.ALFA.BASAL","TNF.ALFA.FINAL","CALCIO.BASAL","CALCIO.FINAL")]
-margarida<-margarida[c("Ordem","Medicamento","FAO.BASAL","FAO.FINAL")]
-# Retirando os pacientes do medicamento Protos
-margarida<-margarida[margarida$Medicamento!="Protos",]
-margarida<-droplevels(margarida)
-
 summary(margarida[,c("FAO.BASAL","FAO.FINAL")])
 
 par(mfrow=c(2,2))
 
-attach(margarida)
 boxplot(data.frame(Basal=FAO.BASAL,Final=FAO.FINAL),main="FAO",ylab="ug/L")
 
 # FAO
