491e5583fe400a326856d3ea1000fdf42b1712b0
Felipe Figueiredo <philsf79@gmail.com>
2015-12-17 14:21:19 -0200

    * scripts/lm.R: Preparando versão "final" das figuras

--- scripts/lm.R
+++ scripts/lm.R
@@ -15,7 +15,7 @@ par(mfrow=c(1,1))
 
 fit.vit<-lm(VITAMINAD.FINAL~VITAMINAD.BASAL)
 summary(fit.vit)
-plot(VITAMINAD.BASAL,VITAMINAD.FINAL)
+plot(VITAMINAD.BASAL,VITAMINAD.FINAL, main="Vitamina D",xlab="Basal (ng/mL)",ylab="Final (ng/mL)")
 abline(fit.vit, lwd=2, col="blue")
 
 dev.off()
@@ -26,37 +26,37 @@ par(mfrow=c(2,3))
 # ADIPONECTINA
 fit.adp<-lm(ADIPONECTINA.FINAL~ADIPONECTINA.BASAL)
 summary(fit.adp)
-plot(ADIPONECTINA.BASAL,ADIPONECTINA.FINAL)
+plot(ADIPONECTINA.BASAL,ADIPONECTINA.FINAL, main="Adiponectina",xlab="Basal (ng/mL)",ylab="Final (ng/mL)")
 abline(fit.adp, lwd=2, col="blue")
 
 # CTX
 fit.ctx<-lm(CTX.FINAL~CTX.BASAL)
 summary(fit.ctx)
-plot(CTX.BASAL,CTX.FINAL)
+plot(CTX.BASAL,CTX.FINAL, main="CTX",xlab="Basal (ng/mL)",ylab="Final (ng/mL)")
 abline(fit.ctx, lwd=2, col="blue")
 
 # FAO
 fit.fao<-lm(FAO.FINAL~FAO.BASAL)
 summary(fit.fao)
-plot(FAO.BASAL,FAO.FINAL)
+plot(FAO.BASAL,FAO.FINAL, main="FAO",xlab="Basal (ug/mL)",ylab="Final (ug/mL)")
 abline(fit.fao, lwd=2, col="blue")
 
 #LEPTINA
 fit.lep<-lm(LEPTINA.FINAL~LEPTINA.BASAL)
 summary(fit.lep)
-plot(LEPTINA.BASAL,LEPTINA.FINAL)
+plot(LEPTINA.BASAL,LEPTINA.FINAL, main="Leptina",xlab="Basal (pg/mL)",ylab="Final (pg/mL)")
 abline(fit.lep, lwd=2, col="blue")
 
 # OSTEOPROTEGERINA
 fit.opg<-lm(OPG.FINAL~OPG.BASAL)
 summary(fit.opg)
-plot(OPG.BASAL,OPG.FINAL)
+plot(OPG.BASAL,OPG.FINAL, main="OPG",xlab="Basal (pg/mL)",ylab="Final (pg/mL)")
 abline(fit.opg, lwd=2, col="blue")
 
 # TNFALFA
 fit.tnf<-lm(TNFALFA.FINAL~TNFALFA.BASAL)
 summary(fit.tnf)
-plot(TNFALFA.BASAL,TNFALFA.FINAL)
+plot(TNFALFA.BASAL,TNFALFA.FINAL, main="TNF-a",xlab="Basal (pg/mL)",ylab="Final (pg/mL)")
 abline(fit.tnf, lwd=2, col="blue")
 
 dev.off()
