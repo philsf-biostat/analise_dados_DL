edd71a42e03f3d1271a0cc12e21c399d9b0f0027
Felipe Figueiredo <philsf79@gmail.com>
2016-01-21 18:17:49 -0200

    Chunks formatados, e código obsoleto movido para o final

--- scripts/diferencas.R
+++ scripts/diferencas.R
@@ -1,3 +1,4 @@
+## Teste de Mann-Whitney pareado ####
 dados.num <- read.table("../dataset/dadosnum.dat")
 attach(dados.num)
 getOption("digits") # default = 7
@@ -7,7 +8,6 @@ vars <- c("VITD","ADP","CTX","FAO","LEP","OPG","TNFalfa")
 desfecho <- c("aumentou","aumentou","diminuiu","diminuiu","diminuiu","aumentou","diminuiu")
 df <- data.frame(Variável=vars, Desfecho=desfecho,"p-valor"=seq(1,7))
 
-## Teste de Mann-Whitney pareado
 df[df$Variável == "VITD",]$p.valor <- wilcox.test(VITAMINAD.FINAL,VITAMINAD.BASAL, paired = T, alternative = "g")$p.value
 df[df$Variável == "ADP",]$p.valor <- wilcox.test(ADIPONECTINA.FINAL,ADIPONECTINA.BASAL, paired = T, alternative = "g")$p.value
 df[df$Variável == "CTX",]$p.valor <- wilcox.test(CTX.FINAL,CTX.BASAL, paired = T, alternative = "l")$p.value
@@ -19,16 +19,8 @@ df[df$Variável == "TNFalfa",]$p.valor <- wilcox.test(TNFALFA.FINAL,TNFALFA.BASA
 write.table(df,file = "../resultados/diferencas.dat")
 rm(df,vars,desfecho)
 
-## Teste t pareado
-t.test(ADIPONECTINA.FINAL,ADIPONECTINA.BASAL, paired = T,alternative = "g")
-t.test(CTX.FINAL,CTX.BASAL, paired = T, alternative = "l")
-#with(margarida.long,t.test(CTX~time, paired=T, alternative="g"))
-t.test(FAO.FINAL,FAO.BASAL, paired = T, alternative = "l")
-t.test(LEPTINA.FINAL,LEPTINA.BASAL, paired = T, alternative = "l")
-t.test(OPG.FINAL,OPG.BASAL, paired = T, alternative = "g")
-t.test(TNFALFA.FINAL,TNFALFA.BASAL, paired = T, alternative = "l")
 
-## (curiosidade) Diferenças por medicamento?
+## (curiosidade) Diferenças por medicamento? ####
 ## t.test(margarida$VITAMINAD.BASAL[Medicamento=="Aclasta"],margarida$VITAMINAD.BASAL[Medicamento=="Fosamax"])
 t.test(margarida$VITAMINAD.FINAL[Medicamento=="Aclasta"],margarida$VITAMINAD.FINAL[Medicamento=="Fosamax"])
 wilcox.test(margarida$VITAMINAD.FINAL[Medicamento=="Aclasta"],margarida$VITAMINAD.FINAL[Medicamento=="Fosamax"])
@@ -45,3 +37,12 @@ wilcox.test(margarida$LEPTINA.FINAL[Medicamento=="Aclasta"],margarida$LEPTINA.FI
 t.test(margarida$ADIPONECTINA.FINAL[Medicamento=="Aclasta"],margarida$ADIPONECTINA.FINAL[Medicamento=="Fosamax"])
 wilcox.test(margarida$ADIPONECTINA.FINAL[Medicamento=="Aclasta"],margarida$ADIPONECTINA.FINAL[Medicamento=="Fosamax"])
 
+## Obsoletos ####
+# ## Teste t pareado
+# t.test(ADIPONECTINA.FINAL,ADIPONECTINA.BASAL, paired = T,alternative = "g")
+# t.test(CTX.FINAL,CTX.BASAL, paired = T, alternative = "l")
+# #with(margarida.long,t.test(CTX~time, paired=T, alternative="g"))
+# t.test(FAO.FINAL,FAO.BASAL, paired = T, alternative = "l")
+# t.test(LEPTINA.FINAL,LEPTINA.BASAL, paired = T, alternative = "l")
+# t.test(OPG.FINAL,OPG.BASAL, paired = T, alternative = "g")
+# t.test(TNFALFA.FINAL,TNFALFA.BASAL, paired = T, alternative = "l")
