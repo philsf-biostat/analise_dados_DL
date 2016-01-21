f9cca8f275d64c5b6af0723909cb2568711d3c41
Felipe Figueiredo <philsf79@gmail.com>
2016-01-21 15:53:32 -0200

    Tabela de correlações agora é automática, datafile salvo.

--- scripts/cor.R
+++ scripts/cor.R
@@ -1,4 +1,15 @@
-## Correlações entre valores basal e final
+## Correlações entre VitD e marcadores (ambos final) ####
+dados.num <- read.table("../dataset/dadosnum.dat")
+cor.fun <- function(x) c(cor.test(VITAMINAD.FINAL,x,method="spearman")$estimate,cor.test(VITAMINAD.FINAL,x,method="spearman")$p.value)
+ct <- vapply(dados.num, cor.fun,c("rho" = 0,"p-valor" = 0))
+ct <- round(ct, digits = 4)
+ct <- t(ct)
+ct<-ct[c("ADIPONECTINA.FINAL","CTX.FINAL","FAO.FINAL","LEPTINA.FINAL","OPG.FINAL","TNFALFA.FINAL"),]
+
+write.table(ct,file = "../resultados/cortest.dat")
+rm(ct,cor.fun)
+
+## Correlações entre valores basal e final ####
 cor.test(VITAMINAD.BASAL,VITAMINAD.FINAL,method="spearman") # Vitamina D
 
 cor.test(ADIPONECTINA.BASAL,ADIPONECTINA.FINAL,method="spearman") # ADIPONECTINA
@@ -8,18 +19,11 @@ cor.test(LEPTINA.BASAL,LEPTINA.FINAL,method="spearman") # LEPTINA
 cor.test(OPG.BASAL,OPG.FINAL,method="spearman") # OSTEOPROTEGERINA
 cor.test(TNFALFA.BASAL,TNFALFA.FINAL,method="spearman") # TNFALFA
 
-## Correlação entre VitD e marcadores (ambos nível final)
-cor(VITAMINAD.FINAL,ADIPONECTINA.FINAL,method="spearman") # ADP
-cor(VITAMINAD.FINAL,CTX.FINAL,method="spearman") # CTX
-cor(VITAMINAD.FINAL,FAO.FINAL,method="spearman") # FAO
-cor(VITAMINAD.FINAL,LEPTINA.FINAL,method="spearman") # LEPTINA
-cor(VITAMINAD.FINAL,OPG.FINAL,method="spearman") # OPG
-cor(VITAMINAD.FINAL,TNFALFA.FINAL,method="spearman") # TNF
-
-## Teste de correlação entre VitD e marcadores (ambos nível final)
-cor.test(VITAMINAD.FINAL,ADIPONECTINA.FINAL,method="spearman") # ADP
-cor.test(VITAMINAD.FINAL,CTX.FINAL,method="spearman") # CTX
-cor.test(VITAMINAD.FINAL,FAO.FINAL,method="spearman") # FAO
-cor.test(VITAMINAD.FINAL,LEPTINA.FINAL,method="spearman") # LEPTINA
-cor.test(VITAMINAD.FINAL,OPG.FINAL,method="spearman") # OPG
-cor.test(VITAMINAD.FINAL,TNFALFA.FINAL,method="spearman") # TNF
+## Obsoletos ####
+# ## Teste de correlação entre VitD e marcadores (ambos nível final)
+# cor.test(VITAMINAD.FINAL,ADIPONECTINA.FINAL,method="spearman") # ADP
+# cor.test(VITAMINAD.FINAL,CTX.FINAL,method="spearman") # CTX
+# cor.test(VITAMINAD.FINAL,FAO.FINAL,method="spearman") # FAO
+# cor.test(VITAMINAD.FINAL,LEPTINA.FINAL,method="spearman") # LEPTINA
+# cor.test(VITAMINAD.FINAL,OPG.FINAL,method="spearman") # OPG
+# cor.test(VITAMINAD.FINAL,TNFALFA.FINAL,method="spearman") # TNF
